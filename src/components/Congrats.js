cde06d7c975927ad02e8a2add7f0c1504735eac4
Jared Brown <jaredbrown1@hotmail.com>
2020-01-27 17:02:22 -0800

    add proptypes for Congrats component

--- src/components/Congrats.js
+++ src/components/Congrats.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 /**
  * Functional react component for congratulatory message
@@ -6,7 +7,7 @@ import React from "react";
  * @param {object} pros - React props.
  * @returns {JSX.Element}
  */
-export default props => {
+const Congrats = props => {
   if (props.success) {
     return (
       <div data-test="component-congrats">
@@ -19,3 +20,9 @@ export default props => {
     return <div data-test="component-congrats" />;
   }
 };
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired
+};
+
+export default Congrats;
